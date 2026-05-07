77b0fd17017d5e4fa5937bdac76d70be07394f04
Thresha Mae Pelaez <pelaezthreshamae18@gmail.com>
2026-05-08 05:19:52 +0800

    Removed all localhost API URLs

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://inventory-management-system1-ptf7.onrender.com/api'
+  apiUrl: 'https://inventory-management-system1-ptf7.onrender.com'
 };
