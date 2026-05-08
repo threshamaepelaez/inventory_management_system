068a437f1d50305aa942183683a8b62dc56295ca
Thresha Mae Pelaez <pelaezthreshamae18@gmail.com>
2026-05-08 16:32:17 +0800

    updated production api url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://inventory-management-system1-ptf7.onrender.com'
+  apiUrl: 'https://inventory-management-system1-ptf7.onrender.com/api'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://inventory-management-system1-ptf7.onrender.com'
+  production: true,
+  apiUrl: 'https://inventory-management-system1-ptf7.onrender.com/api'
 };
